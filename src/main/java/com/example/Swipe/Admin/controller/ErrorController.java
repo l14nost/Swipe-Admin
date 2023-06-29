e432565f26b79e9e9e1a53b4d8d16bfde3cd4a89
l14nost <amirbanor@gmail.com>
2023-06-29 16:01:51 +0300

    fix bug

---
+++ src/main/java/com/example/Swipe/Admin/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package com.example.Swipe.Admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    @RequestMapping("/404")
+    public String handle404() {
+        return "error/404";
+    }
+}
