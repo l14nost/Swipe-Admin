b2d0a2911fbfe2b5968138021db6124c4f218bca
l14nost <amirbanor@gmail.com>
2023-07-11 20:23:48 +0300

    fix links

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT into user(iduser,mail,password,role,black_list)
-values (1,'admin@gmail.com','$2a$10$BNADVq8ingPQ5f0.MkDKT.MdMgJebBoyNEgsguA/Ad87Jg7fj8xNW','ADMIN',false)
+-- INSERT into user(iduser,mail,password,role,black_list)
+-- values (1,'admin@gmail.com','$2a$10$BNADVq8ingPQ5f0.MkDKT.MdMgJebBoyNEgsguA/Ad87Jg7fj8xNW','ADMIN',false)
